39817e68478e10dd084ef2e098b8257834ef10a2
Lupita255 <75394038+Lupita255@users.noreply.github.com>
2021-02-26 17:39:41 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,50 +1,61 @@
-var balloon;
-var bg,movement;
+var balloon,balloonImg;
+var database;
+var height;
 
 function preload(){
-  bg = loadImage("background.png");
-  movement=loadAnimation("balloon1.png","balloon2.png","balloon3.png");
-}
+   bg =loadImage("background.png");
+   balloonImg=loadAnimation("balloon1.png","balloon2.png","balloon3.png");
+  }
 
 function setup() {
-  createCanvas(800,500);
-  balloon.addAnimation(movement);
-  var balloonPosition= database.ref('balloon/position');
-  balloonPosition.on("value", readPosition,showError)
+  createCanvas(500,500);
+  database = firebase.database();
+
+  balloon=createSprite(250,650);
+  balloon.addAnimation("hotAirBalloon",balloonImg);
+
+  var balloonHeight=database.ref('balloon/height');
+  balloonHeight.on("value",readHeight, showError);
 }
 
 function draw() {
-  background(bg); 
-  textSize(20);
-  stroke("black");
-  text("Use arrow keys to move the balloon",10,30); 
+  background(bg);
 
   if(keyDown(LEFT_ARROW)){
-    balloon.x = balloon.x -10
-}
-else if(keyDown(RIGHT_ARROW)){
-    balloon.x = balloon.x +10
-}
-else if(keyDown(UP_ARROW)){
-    balloon.y = balloon.y -10
-}
-else if(keyDown(DOWN_ARROW)){
-  balloon.y = balloon.y +10
-}
+    updateHeight(-10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImg);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    updateHeight(10,0);
+    balloon.addAnimation("hotAirBalloon",balloonImg);
+  }
+  else if(keyDown(UP_ARROW)){
+    updateHeight(0,-10);
+    balloon.addAnimation("hotAirBalloon",balloonImg);
+    balloon.scale=balloon.scale -0.005;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    updateHeight(0,+10);
+    balloon.addAnimation("hotAirBalloon",balloonImg);
+    balloon.scale=balloon.scale+0.005;
+  }
+
   drawSprites();
+  
 }
 
+
 function updateHeight(x,y){
-  database.ref('balloon/position').set({
-    'x':balloon.x+x,
-    'y':balloon.y+y
+  database.ref('balloon/height').set({
+    'x': height.x + x ,
+    'y': height.y + y
   })
 }
 
 function readHeight(data){
-  position=data.val();
-  balloon.x=position.x;
-  balloon.y=position.y;
+  height = data.val();
+  balloon.x = height.x;
+  balloon.y = height.y;
 }
 
 function showError(){
